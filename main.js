fa102eaa3fe3c5e930892a00f26d7ef8ae736b16
elkorn <helluinster@gmail.com>
2013-12-29 16:23:32 +0100

    [codiag] Allowing to create connections between bubbles.

--- main.js
+++ main.js
@@ -1,78 +1,47 @@
-/* global fabric, requestAnimationFrame, _*/
+/* global fabric _*/
 "use strict";
 
 var canvas = new fabric.Canvas("c");
-canvas.setWidth(800);
-canvas.setHeight(600);
+canvas.setWidth(1920);
+canvas.setHeight(1080);
 var size = 100;
+
 var x = new codiag.Bubble({
     text: "lorem ipsum dolor sit amet\nthis is a multiline text\nit should be centered",
+    left: 10,
+    top: 10,
+    canvas: canvas
+});
+var y = new codiag.Bubble({
+    text: "the second element\nwith multiline text",
     left: 300,
     top: 300,
-    fill: "yellow",
-    canvas: canvas});
-// var objs = [
-//     new fabric.Rect({ left: 100, top: 100, width: size, height: size, fill: "#BADA55" }),
-//     new fabric.Triangle({ left: 130 + size, top: 100, width: size, height: size, fill: "blue" })
-// ];
-// 
-// window.codiag = {};
-// 
-// fabric.Image.fromURL("http://placekitten.com/200/300", function(img) {
-//     img.scale(0.5).setFlipX(true);
-//     canvas.add(img);
-//     animateSeparately(img);
-// });
-// 
-// var text = new fabric.Text("Test text", {
-//     fontFamily: "Impact"
-// });
-// 
-// canvas.add(text);
-// 
-// objs[0].set({strokeWidth: 5, stroke: "rgba(200,200,200,0.5)"});
-// 
-// function addToCanvas(object) {
-//     canvas.add(object);
-//     return object;
-// }
-// 
-// function changeAngle(obj) {
-//     obj.set({angle: obj.angle + 1 % 360 });
-// }
-// 
-// function rotateObjs() {
-//     objs.forEach(rotate);
-//     canvas.renderAll();
-//     requestAnimationFrame(rotateObjs);
-// }
-// 
-// function rotate(obj) {
-//     changeAngle(obj);
-//     return obj;
-// }
-// 
-// function animateSeparately(obj) {
-//     obj.animate("angle", "+=1", {
-//         onChange: _.compose(
-//                       requestAnimationFrame.bind(window, animateSeparately.bind(null, obj)),
-//                       canvas.renderAll.bind(canvas))
-//     });
-// }
-// 
-// function flipY(obj) {
-//     obj.set({flipY: !obj.flipY});
-//     return obj;
-// }
-// 
-// var transforms = [addToCanvas]; 
-// 
-// function createPipeline(fns){
-//     return _.compose.apply(_, fns.reverse());
-// }
-// 
-// var startFn = createPipeline(transforms);
-// objs.forEach(startFn);
-// 
-// rotateObjs();
-// 
+    canvas: canvas
+});
+
+var xcp = x.shape.getCenterPoint();
+var ycp = y.shape.getCenterPoint();
+
+function updateInputConnectionCoords(result) {
+    return function(connection) {
+        connection.set({
+            x2: result.left,
+            y2: result.top
+        });
+    };
+}
+
+function updateOutputConnectionCoords(result) {
+    return function(connection) {
+        connection.set({
+            x1: result.left,
+            y1: result.top
+        });
+    };
+}
+
+codiag.createConnection({
+    from: x,
+    to: y,
+    canvas: canvas
+});
