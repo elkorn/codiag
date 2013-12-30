7378e6c8f84826384a37f0fc6e504c676857b656
elkorn <helluinster@gmail.com>
2013-12-30 17:42:22 +0100

    [codiag] Removing unnecessary files.

--- CubicBezier.js
+++
@@ -1,8 +0,0 @@
-(function(window, codiag, fabric, undefined) {
-    "use strict";
-    codiag.CubicBezier = function(from, to, p3) {
-    // https://lh4.googleusercontent.com/VBLm-yfTBU26asj7adYjXCARb6304178u4fFr2Cqw-35CKBJkSL-LjuNVnkaQ7YyH-9-37TD90s
-
-    
-    };
-})(window, window.codiag, window.fabric);
