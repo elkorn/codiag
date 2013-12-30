f3380ccd98a84d93635e416f8f9545cd78a1b11d
elkorn <helluinster@gmail.com>
2013-12-30 13:02:15 +0100

    [codiag] #8 Enable creating standalone bubbles

--- static/diagram/diagram.js
+++ static/diagram/diagram.js
@@ -1,7 +1,7 @@
 (function(window, fabric, codiag, undefined) {
     "use strict";
 
-    var currentlyEditedBubble;
+    // var currentlyEditedBubble;
     var bubbles = {};
     var connections = {};
 
@@ -9,9 +9,30 @@
         selection: false
     });
 
+    function uuid() {
+        var result;
+        var existingUuids = Object.keys(bubbles).concat(Object.keys(connections));
+        while (existingUuids.indexOf((result = Math.uuid())) !== -1) {}
+
+        return result;
+    }
+
     canvas.setWidth(1920);
     canvas.setHeight(1080);
 
     codiag.canvas = canvas;
+
+
+    codiag.createBubble = function(shapeOptions, connections) {
+        var result = new codiag.Bubble(shapeOptions, connections);
+        var id = uuid();
+        result.shape.id = id;
+        bubbles[id] = result;
+        return result;
+    };
+
+    codiag.getBubble = function(uuid) {
+        return bubbles[uuid];
+    };
 
 })(window, window.fabric, window.codiag = {});
