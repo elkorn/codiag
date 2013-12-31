81336b0e65d563b232a97fb7ba173abe2e070c5a
elkorn <helluinster@gmail.com>
2013-12-31 18:30:07 +0100

    [codiag] separating button functionality wireup

--- static/diagram/main.js
+++ static/diagram/main.js
@@ -1,18 +1,19 @@
 /* global fabric _*/
+
 "use strict";
 
 var canvas = codiag.canvas;
 var x = codiag.createBubble({
     text: "lorem ipsum dolor sit amet\nthis is a multiline text\nit should be centered",
     left: 10,
-    top: 10,
+    top: 100,
     canvas: canvas
 });
 
 var y = codiag.createBubble({
     text: "the second element\nwith multiline text",
     left: 300,
-    top: 300,
+    top: 400,
     canvas: canvas
 });
 
