8166e098146e17da14f9f342fada80d27a5c1de4
Dawid Mazur <dmazurinf@gmail.com>
2014-01-05 11:33:20 +0100

    Improved rooms view, added room addition possibility

--- app/scripts/controllers/rooms.js
+++ app/scripts/controllers/rooms.js
@@ -3,6 +3,25 @@
 angular.module("codiagApp")
     .controller("RoomsCtrl", function($scope, RoomsService/*, $route*/) {
         $scope.rooms /*= window.rs*/ = RoomsService;
+        $scope.newRoom = false;
+        
+        $scope.showNewRoomForm = function() {
+            $scope.newRoom = true;
+        };
+        
+        $scope.hideNewRoomForm = function() {
+            $scope.newRoom = false;
+            $scope.newRoomName = '';
+        };
+        
+        $scope.createNewRoom= function() {
+            $scope.rooms.$add(
+                { name: $scope.newRoomName, id: Math.uuid(), diagram: { bubbles: [], connections: [] }}
+            );
+            
+            $scope.newRoom = false;
+            $scope.newRoomName = '';
+        };
 
         $scope.getRoomIndex = function(roomNumber) {
             return RoomsService.$getIndex()[roomNumber];
